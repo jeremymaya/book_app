c8d0779f7c967c27efe1de72b569ffc653860316
Corey Marchand <corey.marchand@me.com>
2019-09-24 11:34:02 -0700

    feature 3 almost done, still need to work on show.ejs

--- server.js
+++ server.js
@@ -2,10 +2,11 @@
 
 // Dependencies
 const express = require('express');
+const superagent = require('superagent');
 
 // Applications
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3002;
 app.listen(PORT, () => console.log(`Listening on port: ${PORT}`));
 
 app.use(express.static('public'));
@@ -18,4 +19,35 @@ app.set('view engine', 'ejs');
 app.get('/', (request, response) => {
   response.render('pages/index');
 })
+
+app.post('/searches', searchForBooks);
+
+function Book(bookObj){
+  const placeHolderImage = 'https://i.imgur.com/J5LVHEL.jpg';
+  this.title = bookObj.volumeInfo.title || 'title infromation not available';
+  this.author = bookObj.volumeInfo.author || 'author information not available';
+  this.description = bookObj.volumeInfo.description || 'no description available';
+  this.image = bookObj.volumeInfo.imageLinks.thumbnail || placeHolderImage;
+}
+
+function searchForBooks(request, response){
+  console.log(request.body.search);
+  const searchingFor = request.body.search[0];
+  const searchingType = request.body.search[1];
+  let url = 'https://www.googleapis.com/books/v1/volumes?q=';
+
+  searchingType === 'title' ? url = url+`inauthor:${searchingFor}` : url = url+`intitle:${searchingFor}`;
+
+  // if(searchingType === 'title'){
+  //   url = url+`inauthor:${searchingFor}`;
+  // } else {
+  //   url = url+`intitle:${searchingFor}`;
+  // }
+  superagent.get(url).then(result =>{
+    const library = result.body.items.map(bookObj => {
+      return new Book(bookObj);
+    })
+    response.send(library);
+  }).catch(error => console.log(error));
+}
 
