07af2c603afdb8325ade0ca59401a09a24107481
jeremiah <jeremiah.kim@hotmail.com>
2019-09-26 13:32:31 -0700

    submitting milestone-01

--- server.js
+++ server.js
@@ -4,6 +4,7 @@
 const express = require('express');
 const superagent = require('superagent');
 const pg = require('pg');
+const methodOverride = require('method-override');
 
 // Environment variable
 require('dotenv').config();
@@ -18,6 +19,14 @@ app.listen(PORT, () => console.log(`Listening on port: ${PORT}`));
 app.use(express.static('public'));
 app.use(express.urlencoded({extended:true}));
 
+app.use(methodOverride ((request, response) => {
+  if(request.body && typeof request.body === 'object' && '_method' in request.body){
+    let method = request.body._method;
+    delete request.body._method;
+    return method;
+  }
+}))
+
 const client = new pg.Client(process.env.DATABASE_URL);
 client.connect();
 client.on('error', err => console.error(err));
@@ -27,49 +36,48 @@ app.set('view engine', 'ejs');
 
 // Routes
 app.get('/', getBooks);
-app.get('/pages/searches/new', (request, response) => {
-  response.render('pages/searches/new');
-});
+app.get('/pages/books/:book_id', searchDetailsIndex);
+app.get('/pages/searches/new', (request, response) => response.render('pages/searches/new'))
 app.post('/searches', searchForBooks);
-app.get('/pages/books/:book_id', getOneBook);
-app.get('/searches/pages/add/:book_index', saveOneBook);
-
+app.get('/searches/pages/books/:tempId', searchDetailsNew);
+app.post('/update/pages/books/:tempId', saveSearchedBook);
 app.use('*', (request, response)=> response.render('pages/error'));
-
-function Book(bookObj, i){
-  const placeHolderImage = 'https://i.imgur.com/J5LVHEL.jpg';
-  this.title = bookObj.volumeInfo.title || 'title infromation not available';
-  this.author = bookObj.volumeInfo.authors || 'author information not available';
-  this.description = bookObj.volumeInfo.description || 'no description available';
-  this.url = bookObj.volumeInfo.imageLinks.thumbnail || placeHolderImage;
-  this.tempId = i;
-}
 
 let bookArray =[];
 
-function saveOneBook(request, response){
-  const bookIndex = request.params.book_index;
-  console.log(bookIndex);
-  console.log('I am in the save One book')
-  let sql = 'INSERT INTO books (title, author, description, url) VALUES ($1, $2, $3, $4);';
-  let values = [bookArray[bookIndex].title, bookArray[bookIndex].author, bookArray[bookIndex].description, bookArray[bookIndex].url];
+function getBooks(reqeust, response) {
+  let sql = 'SELECT * FROM books;'
 
-  client.query(sql, values);
+  return client
+    .query(sql)
+    .then(result => {
+      console.log(result.rows.length);
+      response.render('pages/index', {searchResults: result.rows})
+    })
+    .catch(error => handleError(error, response));
+}
+
+function searchDetailsIndex (request, response) {
+  let sql = 'SELECT * FROM books WHERE id=$1;';
+  let values = [request.params.book_id];
+
+  return client
+    .query(sql, values)
+    .then(result => response.render('pages/books/show', {book: result.rows[0]}))
+    .catch(error => handleError(error, response));
 }
 
 function searchForBooks(request, response){
-  console.log(request.body.search);
   const searchingFor = request.body.search[0];
   const searchingType = request.body.search[1];
   let url = 'https://www.googleapis.com/books/v1/volumes?q=';
 
   searchingType === 'title' ? url = url+`intitle:${searchingFor}` : url = url+`inauthor:${searchingFor}`;
 
-
   superagent
     .get(url)
     .then(result => {
-      let i = 0;
+      let i = -1;
       return result.body.items.map(bookObj => {
         i++;
         return new Book(bookObj, i)
@@ -82,27 +90,30 @@ function searchForBooks(request, response){
     .catch(error => handleError(error, response));
 }
 
+function searchDetailsNew (request, response) {
+  console.log(bookArray[request.params.tempId]);
+  response.render('pages/books/show', {book: bookArray[request.params.tempId]})
+}
+
+function saveSearchedBook (request, response){
+  const bookIndex = request.params.tempId;
+  const sql = 'INSERT INTO books (author, title, isbn, url, description) VALUES ($1, $2, $3, $4, $5);';
+  const values = [bookArray[bookIndex].author, bookArray[bookIndex].title, bookArray[bookIndex].isbn, bookArray[bookIndex].url, bookArray[bookIndex].description];
+
+  client.query(sql, values).then(response(getBooks));
+}
+
+
+function Book(bookObj, i){
+  const placeHolderImage = 'https://i.imgur.com/J5LVHEL.jpg';
+  this.title = bookObj.volumeInfo.title || 'title infromation not available';
+  this.author = bookObj.volumeInfo.authors || 'author information not available';
+  this.description = bookObj.volumeInfo.description || 'no description available';
+  this.url = bookObj.volumeInfo.imageLinks.thumbnail || placeHolderImage;
+  this.isbn = bookObj.volumeInfo.industryIdentifiers[0].identifier;
+  this.tempId = i;
+}
+
 function handleError(error, response){
   response.render('pages/error');
-}
-
-function getBooks(reqeust, response) {
-  let sql = 'SELECT * FROM books;'
-  return client
-    .query(sql)
-    .then(result => {
-      console.log(result.rows.length);
-      response.render('pages/index', {searchResults: result.rows})
-    })
-    .catch(error => handleError(error, response));
-}
-
-function getOneBook (request, response) {
-  let sql = 'SELECT * FROM books WHERE id=$1;';
-  let values = [request.params.book_id];
-
-  return client
-    .query(sql, values)
-    .then(result => response.render('pages/books/show', {book: result.rows[0]}))
-    .catch(error => handleError(error, response));
 }
